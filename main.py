510c50fa90c28022543da53fd094677bc678196d
LilyGrey <Samantha@Samanthas-MacBook-Air.local>
2015-10-23 22:31:02 -0700

    Altered point call -> speed runtime

--- main.py
+++ main.py
@@ -7,39 +7,45 @@ y_data=[]
 LATCOEF = 168680.5064887113
 LONGCOEF = -5640.630632788689
 
-def get_points(filter):
-    global x_data, y_data
-    for n in grab_portland_crime_data(size=200000):
+def color_by_type(type):
+    if (type=="Drugs"):
+        return "ro"
+    if (type=="Liquor Laws"):
+        return "rs"
+    if (type=="Forgery"):
+        return "mo"
+    if (type=="Fraud"):
+        return "ms"
+    if (type=="Trespass"):
+        return "cs"
+    if (type=="Vandalism"):
+        return"co"
+    if (type=="Larceny"):
+        return "go"
+    if (type=="Robbery"):
+        return "gs"
+    if (type=="Motor Vehicle Theft"):
+        return "bo"
+    if (type=="Burglary"):
+        return "bs"
+    if (type=="Aggravated Assault"):
+        return "ks"
+    if (type=="Assault, Simple"):
+        return "ko"
+    return "k."
 
-        try:
-            x = int(n[-2])
-            y = int(n[-1])
-        except:
-            continue
-        if n[3] == filter:
-            x_data = x_data + [x/LATCOEF]
-            y_data = y_data + [y/LONGCOEF]
+for n in grab_portland_crime_data(size=20000):
+    try:
+        x = int(n[-2])
+        y = int(n[-1])
+        crime = n[3]
+    except:
+        continue
+    x_data = [x/LATCOEF]
+    y_data = [y/LONGCOEF]
+    plt.plot(x_data, y_data, color_by_type(crime))
 
-def plot_by_type( type, color ):
-    global x_data, y_data
-    x_data=[]
-    y_data=[]
-    get_points(type)
-    plt.plot(x_data, y_data, color)
-
-plot_by_type( "Aggravated Assault", "ks" )
-plot_by_type( "Assault, Simple", "ko" )
-plot_by_type( "Burglary", "bs" )
-plot_by_type( "Drugs", "ro" )
-plot_by_type( "Forgery", "mo" )
-plot_by_type( "Fraud", "ms" )
-plot_by_type( "Larceny", "go" )
-plot_by_type( "Liquor Laws", "rs" )
-plot_by_type( "Motor Vehicle Theft", "bo" )
-plot_by_type( "Robbery", "gs" )
-plot_by_type( "Trespass", "cs" )
-plot_by_type( "Vandalism", "co" )
 
 #plt.axis(get_bounds(grab_portland_crime_data()))
-plt.axis([7600000, 7720000, 620000, 740000])
+plt.axis([7600000 / LATCOEF, 7720000 / LATCOEF, 620000 / LONGCOEF, 740000 / LONGCOEF])
 plt.show()
