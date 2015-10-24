d46e5761d57810303b6b1a2a38455017f6a22b9d
P. R. Kumar <prkumar@128.92.110.137.dynamic.ucsd.edu>
2015-10-23 18:41:00 -0700

    updated request.py

--- request.py
+++ request.py
@@ -30,6 +30,7 @@ def request(query, user, pw):
     data['query'] = query
     data['queryBands'] = queryBands
     data['format'] = 'array'
+    data['rowLimit'] = 1000
 
     # Build request.
     req = urllib2.Request(url, json.dumps(data), headers)
@@ -58,17 +59,42 @@ if __name__ == '__main__':
     wsUser = 'hack_user11'
     wsPass = 'tdhackathon'
 
+    # get data
+    # response = request('select * from crime_data.portland_crime', wsUser, wsPass)
+
     # printing portland crime
-    print json.dumps(request('select * from crime_data.portland_crime', wsUser, wsPass), indent=4, sort_keys=True)
+    # print json.dumps(response, indent=4, sort_keys=True)
+
+    # saving data
+    # with open('portland_crime_data_1000', 'w') as stream:
+    #     json.dump(response, stream)
+    # print('dumped!')
+
+    with open('portland_crime_data_1000', 'r') as stream:
+        response = json.load(stream)
+
 
     # looking for min, max x and y
-    xs = []
-    ys = []
-    for data in request('select * from crime_data.portland_crime', wsUser, wsPass)["results"][0]["data"]:
-        xs.append(data[-2])
-        ys.append(data[-1])
+    data = response['results'][0]['data']
+    xs_min = xs_max = data[0][-2]
+    ys_min = ys_max = data[0][-1]
+    for n in data[1:]:
+        try:
+            x = float(n[-1])
+            y = float(n[-2])
+        except:
+            continue
 
-    print len(xs), len(ys)
+        if x < xs_min:
+            xs_min = x
+        if x > xs_max:
+            xs_max = x
+        if y < ys_min:
+            ys_min = y
+        if y > ys_max:
+            ys_max = y
+
+    print xs_min, xs_max, ys_min, ys_max
 
 
 
