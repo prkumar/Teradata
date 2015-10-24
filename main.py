0e0ce03b2cd5758cbe1f068a4b881b5688e8c5c3
LilyGrey <Samantha@Samanthas-MacBook-Air.local>
2015-10-23 20:50:29 -0700

    Altered color of crimes, added more data points

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ y_data=[]
 
 def get_points( filter ):
     global x_data, y_data
-    for n in grab_portland_crime_data(size=5000):
+    for n in grab_portland_crime_data(size=10000):
         try:
             x = int(n[-2])
             y = int(n[-1])
@@ -25,19 +25,18 @@ def plot_by_type( type, color ):
     get_points(type)
     plt.plot( x_data, y_data, color)
 
-plot_by_type( "Aggravated Assault", "bo" )
-plot_by_type( "Assault, Simple", "co" )
-plot_by_type( "Burglary", "ko" )
-plot_by_type( "Drugs", "ys" )
-plot_by_type( "Forgery", "go" )
-plot_by_type( "Fraud", "yo" )
-plot_by_type( "Larceny", "mo" )
+plot_by_type( "Aggravated Assault", "ks" )
+plot_by_type( "Assault, Simple", "ko" )
+plot_by_type( "Burglary", "bs" )
+plot_by_type( "Drugs", "ro" )
+plot_by_type( "Forgery", "mo" )
+plot_by_type( "Fraud", "ms" )
+plot_by_type( "Larceny", "go" )
 plot_by_type( "Liquor Laws", "rs" )
-plot_by_type( "Motor Vehicle Theft", "ks" )
+plot_by_type( "Motor Vehicle Theft", "bo" )
 plot_by_type( "Robbery", "gs" )
 plot_by_type( "Trespass", "cs" )
-plot_by_type( "Vandalism", "ms" )
+plot_by_type( "Vandalism", "co" )
 
 plt.axes(get_bounds(grab_portland_crime_data()))
 plt.show()
-
